5e748ccfce6739c88208c82920bcc87060646b37
Abhishek Yadav <abhishekyadav84595@gmail.com>
2025-04-13 23:18:05 +0530

    changed folder structure

--- blogapp/src/main/java/com/code/blog/blogapp/repository/BlogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.code.blog.blogapp.repository;
-
-import com.code.blog.blogapp.entity.Blog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BlogRepository extends JpaRepository<Blog, Long> {
-}
