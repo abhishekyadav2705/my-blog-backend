7483cbff9eb06ad03964cd7616f4ca424ec235d3
Abhishek Yadav <abhishekyadav84595@gmail.com>
2025-04-13 22:32:28 +0530

    changes in controller

--- blogapp/src/main/java/com/code/blog/blogapp/controller/BlogController.java
+++ blogapp/src/main/java/com/code/blog/blogapp/controller/BlogController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = {"http://localhost:3000", "https://abhishekyadavsblogs.netlify.app"})
 @RestController
 @RequestMapping("/api/blogs")
 public class BlogController {
